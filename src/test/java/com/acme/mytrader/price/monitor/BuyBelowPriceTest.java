a90e6906ee89adaae9b1ba3441a32135d7f7b047
Qhaios <rilwanj@hotmail.com>
2022-01-17 11:19:05 +0000

    Applied the correct indentations in the files

--- src/test/java/com/acme/mytrader/price/monitor/BuyBelowPriceTest.java
+++ src/test/java/com/acme/mytrader/price/monitor/BuyBelowPriceTest.java
@@ -23,250 +23,250 @@ import com.acme.mytrader.price.monitor.BuyBelowPrice;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BuyBelowPriceTest {
-	
+
 	private static final String TEST_STOCK_2_MONITOR = "IBM";
 	private static final String TEST_OTHER_STOCK_2_MONITOR = "GOOG";
 	private static final double TEST_PRICE_2_MONITOR = 55;
 	private static final int TEST_VOLUME_2_MONITOR = 55;
-	
+
 	@Mock
 	private ExecutionService executionService;
 
 	private BuyBelowPrice buyAtPriceListener;
-	
+
 	@Before
 	public void setUp() {
 		buyAtPriceListener = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
 	}
-    
-    /**
-     * Test Price Listener Constructor handles malformed arguments
-     */
+
+	/**
+	 * Test Price Listener Constructor handles malformed arguments
+	 */
 	@Test
-    public void testCheckOfNullorNaNListenerCriteria() {
+	public void testCheckOfNullorNaNListenerCriteria() {
 		// Checks if the stock is null, if so should throw NullPointerException
-		
+
 		try {
 			new BuyBelowPrice(null, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService);
-			
+
 			fail("Should have thrown NullPointerException");
 		} catch(NullPointerException ex) {
 			assertEquals("Security was null", ex.getMessage());
 		}
-		
+
 		// Checks if the price is not a number, if so should throw ArithmeticException
-		
+
 		try {
 			new BuyBelowPrice(TEST_STOCK_2_MONITOR, Double.NaN, TEST_VOLUME_2_MONITOR, executionService);
-			
+
 			fail("Should have thrown ArithmeticException");
 		} catch(ArithmeticException ex) {
 			assertEquals("Price is not a number", ex.getMessage());
 		}
-		
+
 		// Checks if the volume is an negative integer, if so throw return ArithmeticException
-		
+
 		try {
 			new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, -1, executionService);
-			
+
 			fail("Should have thrown ArithmeticException");
 		} catch(ArithmeticException ex) {
 			assertEquals("Volume shouldn't be less than 0", ex.getMessage());
 		}
-		
+
 		// Checks if the executionService is null, if so throw return NullPointerException
-		
+
 		try {
 			new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, -1, executionService);
-			
+
 			fail("Should have thrown ArithmeticException");
 		} catch(ArithmeticException ex) {
 			assertEquals("Volume shouldn't be less than 0", ex.getMessage());
 		}
-		
+
 		// Checks if when the arguments are value, it throws no exception
-    	
+
 		try {
 			new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-    }
-    
-    /**
-     * Test Price Listener's stock update checker handles every potential argument correctly
-     */
+	}
+
+	/**
+	 * Test Price Listener's stock update checker handles every potential argument correctly
+	 */
 	@Test
-    public void testInvocationOfCanCheckPriceFor() {
-		
+	public void testInvocationOfCanCheckPriceFor() {
+
 		// Checks canCheckPriceFor handles null without throwing an exception
-		
+
 		try {
 			buyAtPriceListener.canCheckPriceFor(null);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-		
+
 		// Checks canCheckPriceFor handles empty strings without throwing an exception
-    	
+
 		try {
 			buyAtPriceListener.canCheckPriceFor("");
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-		
+
 		// Checks canCheckPriceFor handles different stock without throwing an exception
-    	
+
 		try {
 			buyAtPriceListener.canCheckPriceFor(TEST_STOCK_2_MONITOR);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-		
+
 		// Checks canCheckPriceFor handles monitored stock without throwing an exception
-    	
+
 		try {
 			buyAtPriceListener.canCheckPriceFor(TEST_STOCK_2_MONITOR);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-    }
-    
-    /**
-     * Test Price Listener's buy order executor handles every potential argument correctly
-     */
+	}
+
+	/**
+	 * Test Price Listener's buy order executor handles every potential argument correctly
+	 */
 	@Test
-    public void testInvocationOfBuyAtPrice() {
-		
+	public void testInvocationOfBuyAtPrice() {
+
 		// Checks canCheckPriceFor handles null without throwing an exception
-		
+
 		try {
 			buyAtPriceListener.buyAtPrice(Double.NaN);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-		
+
 		// Checks canCheckPriceFor handles empty strings without throwing an exception
-    	
+
 		try {
 			buyAtPriceListener.canCheckPriceFor("");
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-		
+
 		// Checks canCheckPriceFor handles different stock without throwing an exception
-    	
+
 		try {
 			buyAtPriceListener.canCheckPriceFor(TEST_STOCK_2_MONITOR);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-		
+
 		// Checks canCheckPriceFor handles monitored stock without throwing an exception
-    	
+
 		try {
 			buyAtPriceListener.canCheckPriceFor(TEST_STOCK_2_MONITOR);
 		} catch(Exception ex) {
 			fail("Shouldn't throw any exceptions");
 		}
-    }
-    
-    /**
-     * Test Price Listener skips checks if the stock (a.k.a security) or stock price are null
-     */
+	}
+
+	/**
+	 * Test Price Listener skips checks if the stock (a.k.a security) or stock price are null
+	 */
 	@Test
-    public void testSkipsOnMalformedPriceUpdates() {
+	public void testSkipsOnMalformedPriceUpdates() {
 		// share price update
-    	
-    	// Null stock
-    	buyAtPriceListener.priceUpdate(null, TEST_PRICE_2_MONITOR -0.1);
-    	
-    	verify(buyAtPriceListener, times(0)).canCheckPriceFor(any(String.class));
-    	
-    	// share another price update
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, Double.NaN);
-    	
-    	verify(buyAtPriceListener, times(0)).canCheckPriceFor(any(String.class));
-    }
-    
-    /**
-     * Test Price Listener Monitors Prices for a specific stock
-     */
-    @Test
-    public void testMonitorsPriceUpdates() {
-    	// share price update
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
-    	
-    	verify(buyAtPriceListener, times(1)).canCheckPriceFor(TEST_STOCK_2_MONITOR);
-    	
-    	// share another price update
-    	
-    	buyAtPriceListener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR + 5);
-    	
-    	verify(buyAtPriceListener, times(1)).canCheckPriceFor(TEST_OTHER_STOCK_2_MONITOR);
-    }
-    
-    /**
-     * Test Price Listener triggers buy only one the stock it is monitoring
-     */
-    @Test
-    public void testBuysOnlyForSpecificStock() {
-    	// share price update for other stock
-    	
-    	buyAtPriceListener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
-    	
-    	verify(buyAtPriceListener, never()).buyAtPrice(any(Double.class));
-    	
-    	// share price update
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
-    	
-    	verify(buyAtPriceListener, times(1)).buyAtPrice(any(Double.class));
-    }
-    
-    /**
-     * Test Price Listener triggers buy only when the price is below the monitored price
-     */
-    @Test
-    public void testBuysOnlyWhenStockPriceUpdateBelowMonitoredPrice() {
-    	// don't buy when update stock price is the same as the monitored one
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR);
-    	
-    	verify(buyAtPriceListener, never()).buyAtPrice(any(Double.class));
-    	
-    	// don't buy when update stock price is above the monitored one
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR + 5);
-    	
-    	verify(buyAtPriceListener, never()).buyAtPrice(any(Double.class));
-    	
-    	// buy when update stock price is below the monitored one
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
-    	
-    	verify(buyAtPriceListener, times(1)).buyAtPrice(any(Double.class));
-    }
-    
-    /**
-     * Test Price Listener only buys a specific set volume at a time (when triggered)
-     */
-    @Test
-    public void testBuysSpecificVolumeWhenTriggered() {
-    	// share price update
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1);
-    	
-    	verify(executionService, times(1)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1, TEST_VOLUME_2_MONITOR);
-    	
-    	// share price update
-    	
-    	buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.5);
-    	
-    	verify(executionService, times(1)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.5, TEST_VOLUME_2_MONITOR);
-    }
-    
-   // Skipping test to trigger buy with no ExecutionService provided, as PriceListener's constructor required a valid ExecutioinService
+
+		// Null stock
+		buyAtPriceListener.priceUpdate(null, TEST_PRICE_2_MONITOR -0.1);
+
+		verify(buyAtPriceListener, times(0)).canCheckPriceFor(any(String.class));
+
+		// share another price update
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, Double.NaN);
+
+		verify(buyAtPriceListener, times(0)).canCheckPriceFor(any(String.class));
+	}
+
+	/**
+	 * Test Price Listener Monitors Prices for a specific stock
+	 */
+	@Test
+	public void testMonitorsPriceUpdates() {
+		// share price update
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
+
+		verify(buyAtPriceListener, times(1)).canCheckPriceFor(TEST_STOCK_2_MONITOR);
+
+		// share another price update
+
+		buyAtPriceListener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR + 5);
+
+		verify(buyAtPriceListener, times(1)).canCheckPriceFor(TEST_OTHER_STOCK_2_MONITOR);
+	}
+
+	/**
+	 * Test Price Listener triggers buy only one the stock it is monitoring
+	 */
+	@Test
+	public void testBuysOnlyForSpecificStock() {
+		// share price update for other stock
+
+		buyAtPriceListener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
+
+		verify(buyAtPriceListener, never()).buyAtPrice(any(Double.class));
+
+		// share price update
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
+
+		verify(buyAtPriceListener, times(1)).buyAtPrice(any(Double.class));
+	}
+
+	/**
+	 * Test Price Listener triggers buy only when the price is below the monitored price
+	 */
+	@Test
+	public void testBuysOnlyWhenStockPriceUpdateBelowMonitoredPrice() {
+		// don't buy when update stock price is the same as the monitored one
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR);
+
+		verify(buyAtPriceListener, never()).buyAtPrice(any(Double.class));
+
+		// don't buy when update stock price is above the monitored one
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR + 5);
+
+		verify(buyAtPriceListener, never()).buyAtPrice(any(Double.class));
+
+		// buy when update stock price is below the monitored one
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.1);
+
+		verify(buyAtPriceListener, times(1)).buyAtPrice(any(Double.class));
+	}
+
+	/**
+	 * Test Price Listener only buys a specific set volume at a time (when triggered)
+	 */
+	@Test
+	public void testBuysSpecificVolumeWhenTriggered() {
+		// share price update
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1);
+
+		verify(executionService, times(1)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1, TEST_VOLUME_2_MONITOR);
+
+		// share price update
+
+		buyAtPriceListener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.5);
+
+		verify(executionService, times(1)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -0.5, TEST_VOLUME_2_MONITOR);
+	}
+
+	// Skipping test to trigger buy with no ExecutionService provided, as PriceListener's constructor required a valid ExecutioinService
 }
--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -23,117 +23,117 @@ import com.acme.mytrader.price.monitor.BuyBelowPrice;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class TradingStrategyTest {
-	
+
 	private static final String TEST_STOCK_2_MONITOR = "IBM";
 	private static final double TEST_PRICE_2_MONITOR = 55;
 	private static final String TEST_OTHER_STOCK_2_MONITOR = "GOOG";
 	private static final double TEST_OTHER_PRICE_2_MONITOR = 23;
 	private static final int TEST_VOLUME_2_MONITOR = 55;
-	
+
 	/**
 	 * third party implementation
 	 */
 	@Mock
 	private PriceSource priceSource;
-	
+
 	/**
 	 * third party implementation
 	 */
 	@Mock
 	private ExecutionService executionService;
-	
+
 	/**
 	 * Using List&lt;PriceListener&gt; instead of Map&lt;String, List&lt;PriceListener&gt;&gt;
 	 * to traverse every PriceListner with little effort 
 	 */
 	private List<PriceListener> priceSourcePriceListeners;
-	
+
 	@Before
 	public void setUp() {
-		
+
 		// mock third implementation of PriceSource
 		priceSourcePriceListeners = new ArrayList<>();
-		
+
 		// mock price listener insertion
-    	doAnswer(invocation -> {
-            Object arg0 = invocation.getArgument(0);
-            
-            // arg0 is always PriceListener 
-            
-            priceSourcePriceListeners.add((PriceListener) arg0);
-            return null;
-        }).when(priceSource).addPriceListener(any(PriceListener.class));
+		doAnswer(invocation -> {
+			Object arg0 = invocation.getArgument(0);
+
+			// arg0 is always PriceListener 
+
+			priceSourcePriceListeners.add((PriceListener) arg0);
+			return null;
+		}).when(priceSource).addPriceListener(any(PriceListener.class));
 	}
-    
-    // skipping add and remove PriceListener tests for PriceSource as it could number of implementation
-	
+
+	// skipping add and remove PriceListener tests for PriceSource as it could number of implementation
+
 	// skipping test of executionService.buy(...) throwing runtime exception
-	
+
 	// All subsequence tests will only be about handling price updates from PriceSource
-    
-    /**
-     * Test malformed stock price update from PriceSource is handled without throwing exception
-     */
-    @Test
-    public void testPriceSourceSendingMalformedPriceUpdate() {
-    	BuyBelowPrice buyAtPriceListener = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
-    	BuyBelowPrice buyAtPriceListener2 = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
-    	BuyBelowPrice buyAtPriceListener3 = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
-    	
-    	priceSource.addPriceListener(buyAtPriceListener);
-    	priceSource.addPriceListener(buyAtPriceListener2);
-    	priceSource.addPriceListener(buyAtPriceListener3);
-    	
-    	List<PriceListener> listeners = priceSourcePriceListeners;
-    	
-    	// share IBM price update of 54
-    	
-    	listeners.forEach(listener -> listener.priceUpdate(null, TEST_PRICE_2_MONITOR -0.1));
-    	
-    	verify(buyAtPriceListener, times(0)).getSecurity(); // Confirms canCheckPriceFor wasn't invoked
-    	
-    	// share GOOG price update of 28
-    	
-    	listeners.forEach(listener -> listener.priceUpdate(TEST_STOCK_2_MONITOR, Double.NaN));
-    	
-    	verify(buyAtPriceListener, times(0)).getSecurity(); // Confirms canCheckPriceFor wasn't invoked
-    }
-    
-    /**
-     * Test Price Listener triggers buy only one the stock it is monitoring
-     */
-    @Test
-    public void testBuysVolumeWhenMonitoredStockDropsBelowPrice() {
-    	BuyBelowPrice buyAtPriceListener = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
-    	BuyBelowPrice otherBuyAtPriceListener = spy(new BuyBelowPrice(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
-    	
-    	priceSource.addPriceListener(buyAtPriceListener);
-    	priceSource.addPriceListener(otherBuyAtPriceListener);
-    	
-    	List<PriceListener> listeners = priceSourcePriceListeners;
-    	
-    	// share IBM price update of 54
-    	
-    	listeners.forEach(listener -> listener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1));
-    	
-    	verify(executionService, times(1)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1, TEST_VOLUME_2_MONITOR);
-    	
-    	// share GOOG price update of 28
-    	
-    	listeners.forEach(listener -> listener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR + 5));
-    	
-    	verify(executionService, times(0)).buy(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR + 5, TEST_VOLUME_2_MONITOR);
-    	
-    	// share IBM price update of 55
-    	
-    	listeners.forEach(listener -> listener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR));
-    	
-    	verify(executionService, times(0)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR);
-    	
-    	// share GOOG price update of 22
-    	
-    	listeners.forEach(listener -> listener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR -1));
-    	
-    	verify(executionService, times(1)).buy(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR -1, TEST_VOLUME_2_MONITOR);
-    }
+
+	/**
+	 * Test malformed stock price update from PriceSource is handled without throwing exception
+	 */
+	@Test
+	public void testPriceSourceSendingMalformedPriceUpdate() {
+		BuyBelowPrice buyAtPriceListener = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
+		BuyBelowPrice buyAtPriceListener2 = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
+		BuyBelowPrice buyAtPriceListener3 = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
+
+		priceSource.addPriceListener(buyAtPriceListener);
+		priceSource.addPriceListener(buyAtPriceListener2);
+		priceSource.addPriceListener(buyAtPriceListener3);
+
+		List<PriceListener> listeners = priceSourcePriceListeners;
+
+		// share IBM price update of 54
+
+		listeners.forEach(listener -> listener.priceUpdate(null, TEST_PRICE_2_MONITOR -0.1));
+
+		verify(buyAtPriceListener, times(0)).getSecurity(); // Confirms canCheckPriceFor wasn't invoked
+
+		// share GOOG price update of 28
+
+		listeners.forEach(listener -> listener.priceUpdate(TEST_STOCK_2_MONITOR, Double.NaN));
+
+		verify(buyAtPriceListener, times(0)).getSecurity(); // Confirms canCheckPriceFor wasn't invoked
+	}
+
+	/**
+	 * Test Price Listener triggers buy only one the stock it is monitoring
+	 */
+	@Test
+	public void testBuysVolumeWhenMonitoredStockDropsBelowPrice() {
+		BuyBelowPrice buyAtPriceListener = spy(new BuyBelowPrice(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
+		BuyBelowPrice otherBuyAtPriceListener = spy(new BuyBelowPrice(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR, executionService));
+
+		priceSource.addPriceListener(buyAtPriceListener);
+		priceSource.addPriceListener(otherBuyAtPriceListener);
+
+		List<PriceListener> listeners = priceSourcePriceListeners;
+
+		// share IBM price update of 54
+
+		listeners.forEach(listener -> listener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1));
+
+		verify(executionService, times(1)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR -1, TEST_VOLUME_2_MONITOR);
+
+		// share GOOG price update of 28
+
+		listeners.forEach(listener -> listener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR + 5));
+
+		verify(executionService, times(0)).buy(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR + 5, TEST_VOLUME_2_MONITOR);
+
+		// share IBM price update of 55
+
+		listeners.forEach(listener -> listener.priceUpdate(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR));
+
+		verify(executionService, times(0)).buy(TEST_STOCK_2_MONITOR, TEST_PRICE_2_MONITOR, TEST_VOLUME_2_MONITOR);
+
+		// share GOOG price update of 22
+
+		listeners.forEach(listener -> listener.priceUpdate(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR -1));
+
+		verify(executionService, times(1)).buy(TEST_OTHER_STOCK_2_MONITOR, TEST_OTHER_PRICE_2_MONITOR -1, TEST_VOLUME_2_MONITOR);
+	}
 }
